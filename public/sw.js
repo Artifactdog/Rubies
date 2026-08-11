ff7811281c48851f014f63bca039ae6798f579e0
Artifactdog <StelveFlame@gmail.com>
2026-08-11 15:24:51 +0700

    Release 2026.8.11.1
    * Fix mobile screen ownership and sticky controls
    
    * Load mobile screen fixes
    
    * Bump version to 2026.8.11.1
    
    * Expose 2026.8.11.1 app version
    
    * Refresh PWA cache for 2026.8.11.1
    
    * Guard mobile screen ownership regressions

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rubies-shell-2026.8.11.0'
+const CACHE_NAME = 'rubies-shell-2026.8.11.1'
 const APP_SHELL = ['/', '/manifest.webmanifest', '/icons/icon-192.png', '/icons/icon-512.png']
 
 self.addEventListener('install', (event) => {
