380736dfed6330c8551367ff475620786de08d32
Artifactdog <StelveFlame@gmail.com>
2026-08-17 13:41:41 +0700

    Refresh PWA cache for 2026.8.17.1

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rubies-shell-2026.8.17.0'
+const CACHE_NAME = 'rubies-shell-2026.8.17.1'
 const APP_SHELL = ['/', '/manifest.webmanifest', '/icons/icon-192.png', '/icons/icon-512.png']
 
 self.addEventListener('install', (event) => {
